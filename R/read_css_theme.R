5d816924d7709880f22ad975793d1c46321c366c
Stefan Fleck <stefan.b.fleck@gmail.com>
2018-08-26 14:31:54 +0200

    better trace logging

--- R/read_css_theme.R
+++ R/read_css_theme.R
@@ -1,4 +1,6 @@
-load_theme <- function(x = "default"){
+load_theme <- function(
+  x = "default"
+){
   stopifnot(is_scalar_character(x))
 
   named_themes <- list(
@@ -7,19 +9,21 @@ load_theme <- function(x = "default"){
     "dark"    = system.file("css", "shed_dark.css", package = "shed")
   )
 
-  if (contains_newline(x)){
-    return(x)
-
-  } else  if (file.exists(x)){
+  if (is_css_file(x)){
+    flog.trace("Reading theme from %s", x)
     return(read_css_theme(x))
 
-  } else if (x %in% preset_themes) {
-    preset_themes[[x]]
+  } else if (is_scalar_character(x) && x %in% names(named_themes)){
+    flog.trace("Reading internal theme from %s", named_themes[[x]])
+    return(read_css_theme(named_themes[[x]]))
+
+  } else if (contains_newline(x)) {
+    flog.trace("Theme directly supplied as character scalar")
+    return(x)
 
   } else {
-    flog.warn("Cannot parse theme. Falling back to default theme")
+    flog.warn("Cannot parse theme. Falling back to default")
     read_css_theme(getOption("shed.css", system.file("css", "shed_dark.css", package = "shed")))
-
   }
 }
 
